d383d4f410029c578a939bee7d2d78a3e5919d0c
Maksim Fedorov <maksfedorovru@gmail.com>
2025-02-06 21:59:55 +0400

    Build Deproxy with ALLOW_HOSTS

--- deproxy/setup.sh
+++ deproxy/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 ALLOW_HOSTS=${ALLOW_HOSTS:-""}
 
@@ -9,8 +10,19 @@ fi
 ALLOWED_DOMAINS=$(echo " $ALLOW_HOSTS" | tr ',' ' ')
 CONFIG_FILE="/etc/squid/squid.conf"
 
-
 sed -i "/acl allowed_sites dstdomain/ s/\(acl allowed_sites dstdomain\)/\1 $ALLOWED_DOMAINS/" "$CONFIG_FILE"
+
+# default behaviour is to launch squid
+if [[ -z ${1} ]]; then
+  if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
+    echo "Initializing cache..."
+    $(which squid) -N -f /etc/squid/squid.conf -z
+  fi
+  echo "Starting squid..."
+  exec $(which squid) -f /etc/squid/squid.conf -NYCd 1
+else
+  exec "$@"
+fi
 
 
 
