e91121d44534e97b392ee4ffefedad39da6cdbd0
Maksim Fedorov <maksfedorovru@gmail.com>
2025-02-06 21:41:46 +0400

    Build Deproxy with ALLOW_HOSTS

--- deproxy/setup.sh
+++ deproxy/setup.sh
@@ -9,5 +9,8 @@ fi
 ALLOWED_DOMAINS=$(echo " $ALLOW_HOSTS" | tr ',' ' ')
 CONFIG_FILE="/etc/squid/squid.conf"
 
+
 sed -i "/acl allowed_sites dstdomain/ s/\(acl allowed_sites dstdomain\)/\1 $ALLOWED_DOMAINS/" "$CONFIG_FILE"
+
+
 
