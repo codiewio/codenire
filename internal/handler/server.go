028ee25d8d6502db9ffa733f9d0fc04703ffa4d5
Maksim Fedorov <maksfedorovru@gmail.com>
2025-02-10 01:55:52 +0400

    Add ide

--- internal/handler/server.go
+++ internal/handler/server.go
@@ -5,8 +5,8 @@
 package handler
 
 import (
-	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -21,32 +21,44 @@ func NewServer(config *Config) (*http.Server, error) {
 		Config: config,
 	}
 
-	h := chi.NewRouter()
-	h.Use(middleware.Recoverer)
-	h.Use(middleware.Throttle(15))
+	router := chi.NewRouter()
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Throttle(15))
 
-	h.Get("/", rootHandler)
+	filesDir := http.Dir("/static")
+	FileServer(router, "/", filesDir)
 
-	h.Group(func(r chi.Router) {
+	router.Group(func(r chi.Router) {
 		r.Post("/run", handler.RunFilesHandler)
 		r.Post("/run-script", handler.RunScriptHandler)
 	})
 
-	h.Group(func(r chi.Router) {
+	router.Group(func(r chi.Router) {
 		r.Get("/actions", handler.ActionListHandler)
 	})
 
 	return &http.Server{
 		Addr:              ":" + config.Port,
 		ReadHeaderTimeout: 5 * time.Second,
-		Handler:           &ochttp.Handler{Handler: h},
+		Handler:           &ochttp.Handler{Handler: router},
 	}, nil
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
 	}
-	_, _ = io.WriteString(w, "Hi from playground\n")
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
