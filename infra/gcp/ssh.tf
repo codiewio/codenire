8255817794ba2ecc2834eb2e4352d8646e0d70b6
Maksim Fedorov <maksfedorovru@gmail.com>
2025-02-10 20:45:15 +0400

    Add GCP infra

--- infra/gcp/ssh.tf
+++
@@ -1,9 +0,0 @@
-resource "tls_private_key" "ed25519_key" {
-  algorithm = "ED25519"
-}
-
-resource "google_compute_project_metadata_item" "ssh_key" {
-  key   = "codenire-ssh"
-  value = "Codenire SSH Key — ${var.environment}:${tls_private_key.ed25519_key.public_key_openssh}"
-  project = var.project
-}
